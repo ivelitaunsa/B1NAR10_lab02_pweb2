5d540a3e59461fab5f991c6bc146b14862e88fca
bryanhancco <bhanccoco@unsa.edu.pe>
2022-05-05 21:07:20 -0500

    Asignando casos al switch

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,7 +1,28 @@
 function getDay() {
 	let date = new Date();
 	let numDay = date.getDay();
+	let day;
 	switch (numDay) {
-
+		case 0:
+    		day = "Domingo";
+    		break;
+  		case 1:
+    		day = "Lunes";
+    		break;
+  		case 2:
+     		day = "Martes";
+    		break;
+  		case 3:
+    		day = "Miercoles";
+    		break;
+  		case 4:
+    		day = "Jueves";
+    		break;
+  		case 5:
+    		day = "Viernes";
+    		break;
+  		case 6:
+    		day = "Sabado";
 	}
+	return day;
 }
