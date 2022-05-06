5deb63a3d35183137249a595b7b680e44e160d4a
Eberth Wilfredo Azurin Zuñiga <eazurin@unsa.edu.pe>
2022-05-05 21:48:21 -0500

    Generando los aletorios enteros que iran en la tabla y verificando que esten bien

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,7 +1,12 @@
 let numElementos;
 let miTabla;
 let sumatoria;
+const numerosTabla = [];
 function generarTabla(){
   numElementos = document.getElementById("numElemntos").value;
   console.log(numElementos);
+  for (let i = 0; i < numElementos; i++) {
+    numerosTabla[i] = Math.floor(Math.random() * 200);
+  }
+  console.log(numerosTabla); 
 }
