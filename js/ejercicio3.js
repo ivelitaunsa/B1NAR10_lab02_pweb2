a7584d94402837b863f669f43133d781d259e2a8
Barbara280801 <bcarrillo@unsa.edu.pe>
2022-05-06 09:05:54 -0500

    considero las constantes para horas minutos segundos y dias

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,1 +1,8 @@
 const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const mins = document.getElementById("mins");
+const seconds = document.getElementById("seconds");
+
+const aqp = '15 Aug 2022';
+
+
