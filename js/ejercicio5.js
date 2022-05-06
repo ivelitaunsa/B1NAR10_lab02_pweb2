664565044cc83e22a3815f395c819d408b87573e
Eberth Wilfredo Azurin Zuñiga <eazurin@unsa.edu.pe>
2022-05-05 22:27:35 -0500

    Para que la tabla se muestre bien se tuvo que crear una variable j para que por cada 5 elemtos cree otra columna, como la variable i empezaba en 0 no funcionaba bien

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -9,9 +9,11 @@ function generarTabla(){
     numerosTabla[i] = Math.floor(Math.random() * 200);
   }
   console.log(numerosTabla); 
+  let j = 0;
   for (let i = 0; i < numerosTabla.length; i++) {
     miTabla = miTabla + "<th>"+numerosTabla[i]+"</th>";
-    if(i % 5 == 0 && i != 0) {
+    j++;
+    if(j % 5 == 0) {
       miTabla = miTabla + "</tr><tr>"
     }
   }
