fa11d3e8509d18dc05140bf79a31eaed380176d4
ivelitaunsa <ivelita@unsa.edu.pe>
2022-05-06 15:56:15 -0500

    Creando base del ejercicio 4 tanto html como js

---
+++ js/ejercicio4.js
@@ -0,0 +1,23 @@
+var input
+let parameters
+let url = ""
+let codigo= ""
+
+function returnCodigo() {
+    //Obteniendo etiquetas con id
+    let box = document.getElementById("box-content")
+    input = document.getElementById("input")
+    //Opteniendo valores y resteando input
+    url = input.value
+    input.value = ""
+    //Haciendo match y extrayendo valores con Regex
+    let expresion = /(https:\/\/)?(meet.google.com\/)(...)-(....)-(...)(.+)?/i
+    parameters = url.match(expresion)
+    if(parameters != null) {
+        //Creando salida
+        codigo = `${parameters[3]}${parameters[4]}${parameters[5]}`
+        box.innerHTML = `<h1>El código es: ${codigo}</h1><button onclick='location.reload()'>Volver</button>`   
+    }
+
+
+}
